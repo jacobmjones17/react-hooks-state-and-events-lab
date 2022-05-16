adf8a3dea8f86b80b010da2eda46dad23ff456f0
Jacob <jacobmjones17@gmail.com>
2022-05-16 17:16:34 -0400

    commit tree bfa8f355ceec06c110045caa1239267c54fb965a

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isClassName, setIsClassName] = useState(true)
+
+  const cartClass = isClassName ? "" : "in-cart"
+  
+  const cartText = isClassName ? "Add to Cart" : "Remove From Cart"
+
+  const changeClassName = () => {
+    setIsClassName(!isClassName)
+  }
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={changeClassName}>{cartText}</button>
     </li>
   );
 }
